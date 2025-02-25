6db421c0653e21b5fd92f4082554868b405eee7d
Viktor Angelov <vikotor05@gmail.com>
2025-02-25 12:54:13 +0200

    Made about page and HibernateConfig.java

---
+++ src/main/java/com/example/virtualwallet/controllers/mvc/HomeAboutMvcController.java
@@ -0,0 +1,27 @@
+package com.example.virtualwallet.controllers.mvc;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeAboutMvcController {
+
+    @GetMapping
+    public String showHomePage() {
+        return "home-page";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about-page";
+    }
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+}
